d38b7f375e2916154ef9d0b8cb73e1bc939a951f
Roberto Guido <bob4job@gmail.com>
2015-12-15 21:56:35 +0100

    script fetch degli eventi da Meetup

---
+++ scripts/fetch_meetup.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+        Monitora gli eventi pubblicati sugli accounts Eventbrite dei gruppi aderenti
+        (cfr. parametro source-eventbrite in _data/groups.yml).
+        Da invocare nello script di aggiornamento come
+        php scripts/fetch_eventbrite.php
+*/
+
+require_once('utils.php');
+
+$groups = readHostedGroups();
+
+foreach($groups as $group) {
+        if (isset($group['source-meetup'])) {
+                $url = sprintf("https://api.meetup.com/2/events?group_urlname=%s", $group['source-meetup']);
+                $resp = doGet($url);
+                $resp = json_decode($resp);
+
+                foreach($resp->results as $event) {
+                        $location = sprintf('%s, %s', $event->venue->name, $event->venue->address_1);
+
+                        $obj = (object)[
+                                'title' => $event->name,
+                                'content' => $event->description,
+                                'time' => $event->time / 1000,
+                                'location' => $location,
+                                'url' => $event->event_url,
+                                'group' => $group
+                        ];
+
+                        saveEvent($obj);
+                }
+        }
+}
--- updateme.sh
+++ updateme.sh
@@ -4,6 +4,7 @@ git pull
 
 php scripts/fetch_facebook.php
 php scripts/fetch_eventbrite.php
+php scripts/fetch_meetup.php
 
 if [ -e /usr/local/rvm/scripts/rvm ]
 then
