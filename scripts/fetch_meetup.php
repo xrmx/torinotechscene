229b1dd605f04fbe7d769e0d6078f18c88123ba0
Roberto Guido <bob4job@gmail.com>
2016-01-12 20:40:22 +0100

    piccola correzione in script fetch da Meetup, per gestire eventi con location non nota

--- scripts/fetch_meetup.php
+++ scripts/fetch_meetup.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-        Monitora gli eventi pubblicati sugli accounts Eventbrite dei gruppi aderenti
-        (cfr. parametro source-eventbrite in _data/groups.yml).
-        Da invocare nello script di aggiornamento come
-        php scripts/fetch_eventbrite.php
+	Monitora gli eventi pubblicati sugli accounts Eventbrite dei gruppi aderenti
+	(cfr. parametro source-eventbrite in _data/groups.yml).
+	Da invocare nello script di aggiornamento come
+	php scripts/fetch_eventbrite.php
 */
 
 require_once('utils.php');
@@ -12,24 +12,28 @@ require_once('utils.php');
 $groups = readHostedGroups();
 
 foreach($groups as $group) {
-        if (isset($group['source-meetup'])) {
-                $url = sprintf("https://api.meetup.com/2/events?group_urlname=%s", $group['source-meetup']);
-                $resp = doGet($url);
-                $resp = json_decode($resp);
+	if (isset($group['source-meetup'])) {
+		$url = sprintf("https://api.meetup.com/2/events?group_urlname=%s", $group['source-meetup']);
+		$resp = doGet($url);
+		$resp = json_decode($resp);
 
-                foreach($resp->results as $event) {
-                        $location = sprintf('%s, %s', $event->venue->name, $event->venue->address_1);
+		foreach($resp->results as $event) {
+			if (property_exists($event, 'venue'))
+				$location = sprintf('%s, %s', $event->venue->name, $event->venue->address_1);
+			else
+				$location = 'Sconosciuto';
 
-                        $obj = (object)[
-                                'title' => $event->name,
-                                'content' => $event->description,
-                                'time' => $event->time / 1000,
-                                'location' => $location,
-                                'url' => $event->event_url,
-                                'group' => $group
-                        ];
+			$obj = (object)[
+				'title' => $event->name,
+				'content' => $event->description,
+				'time' => $event->time / 1000,
+				'location' => $location,
+				'url' => $event->event_url,
+				'group' => $group
+			];
 
-                        saveEvent($obj);
-                }
-        }
+			saveEvent($obj);
+		}
+	}
 }
+
