f5ed5dbb99af642a4e59f6dd9613293a17a4c79a
Roberto Guido <bob4job@gmail.com>
2016-01-16 18:52:09 +0100

    URL all'evento nel tweet automatico di segnalazione. Closes #3

--- scripts/post_twitter.php
+++ scripts/post_twitter.php
@@ -12,34 +12,44 @@ require_once('utils.php');
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 $connection = new TwitterOAuth(
-        $conf['twitter']['consumer_key'],
-        $conf['twitter']['consumer_secret'],
-        $conf['twitter']['access_token'],
-        $conf['twitter']['access_token_secret']
+	$conf['twitter']['consumer_key'],
+	$conf['twitter']['consumer_secret'],
+	$conf['twitter']['access_token'],
+	$conf['twitter']['access_token_secret']
 );
 
 $cachefile = 'scripts/cache/tweeted.txt';
 $cached = file($cachefile);
 
 $events = futureEvents();
+$url_length = 0;
 
 foreach($events as $event) {
-        $testable = sprintf("%s %s\n", $event['date'], $event['title']);
-        if (array_search($testable, $cached) !== false)
-                continue;
+	$testable = sprintf("%s %s\n", $event['date'], $event['title']);
+	if (array_search($testable, $cached) !== false)
+		continue;
 
-        file_put_contents($cachefile, $testable, FILE_APPEND);
-        $date = date('d/m: ', strtotime($event['date']));
+	file_put_contents($cachefile, $testable, FILE_APPEND);
+	$date = date('d/m: ', strtotime($event['date']));
 
-        $append = '';
-        $group = groupByName($event['host']);
-        if ($group != null && isset($group['twitter']))
-                $append = sprintf(' /cc @' . $group['twitter']);
+	if ($url_length == 0) {
+		$twitter_conf = $connection->get("help/configuration");
+		// Aggiungo + 3 caratteri per lo spazio occupato dal
+		// trattino di separazione e spazi annessi
+		$url_length = $twitter_conf->short_url_length_https + 3;
+	}
+	$append_url = sprintf(' - %s', $event['link']);
 
-        $limit = 140 - (strlen($date) + strlen($append));
-        $title = substr($event['title'], 0, $limit);
+	$append = '';
+	$group = groupByName($event['host']);
+	if ($group != null && isset($group['twitter']))
+		$append .= sprintf(' /cc @' . $group['twitter']);
 
-        $tweet = sprintf('%s%s%s', $date, $title, $append);
-        $connection->post("statuses/update", array("status" => $tweet));
-        sleep(1);
+	$limit = 140 - (strlen($date) + $url_length + strlen($append));
+	$title = substr($event['title'], 0, $limit);
+
+	$tweet = sprintf('%s%s%s%s', $date, $title, $append_url, $append);
+	$connection->post("statuses/update", array("status" => $tweet));
+	sleep(1);
 }
+
