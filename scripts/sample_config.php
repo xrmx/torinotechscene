128e010ba3acc4a3d7ffb7e9b180a2e9a5a22cd4
Roberto Guido <bob4job@gmail.com>
2018-07-01 11:55:08 +0200

    configurazione di esempio

---
+++ scripts/sample_config.php
@@ -0,0 +1,22 @@
+<?php
+
+$conf = [
+	'facebook' => [
+		'client_id'			=> '',
+		'client_secret'		=> ''
+	],
+	'eventbrite' => [
+		'token'				=> ''
+	],
+	'twitter' => [
+		'consumer_key'			=> '',
+		'consumer_secret'		=> '',
+		'access_token'			=> '',
+		'access_token_secret'	=> ''
+	],
+	'telegram' => [
+		'channels'              => [],
+		'token'                 => '' 
+	]
+];
+
